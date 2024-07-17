587658bab6c90e3587664b1e7cadb6e61c9ce98c
Komal-kalyanraman <komal.kalyanraman@gmail.com>
2024-07-17 14:11:16 +0530

    organized code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# browserUI_kuksa
+# Browser UI for kuksa
+
+In this project I created a browser UI. This UI is used to generate vehicle data. Generated data is sent to Kuksa broker. Developers can receive this data and build applications around it.
+
+Need for this project, its working and code explanation has been provided in 3 different videos. Their links are as follows.
--- server/flask_backend.py
+++ server/flask_backend.py
@@ -6,16 +6,10 @@ from io import BytesIO
 from PIL import Image
 from kuksa_client.grpc import VSSClient
 from kuksa_client.grpc import Datapoint
-from driver_attention_monitor import process_image
+from driver_monitoring_system import process_image
 
 app = Flask(__name__)
 CORS(app)
-
-@app.route('/api/time', methods=['GET'])
-def currentTime():
-    now = datetime.datetime.now()  # Step 2: Get current date and time
-    formatted_now = now.strftime('%Y-%m-%d %H:%M:%S')  # Step 3: Format the date and time
-    return jsonify(time=formatted_now)  # Step 4: Return the formatted time
 
 @app.route('/api/slider', methods=['POST'])
 def handle_slider():
